2579bcae6bc3b69af27b52054b1ece2caec369c8
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-08-20 20:17:23 -0300

    Inicializando componentes para as rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { ResultComponent } from './view/result/result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'result', component: ResultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HomeComponent } from './view/home/home.component';
+import { ResultComponent } from './view/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
