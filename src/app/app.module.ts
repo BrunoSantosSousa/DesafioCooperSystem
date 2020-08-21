6549620282e258ff6e396824af3293c14a8d76f8
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-08-21 17:14:32 -0300

    Instalando dependencias do ngrx para os effects e devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HomeComponent } from './view/home/home.component';
 import { SearchComponent } from './view/search/search.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { RepositoryComponent } from './components/repository/repository.component';
+
+import { reducers } from './store';
+import { EffectsModule } from '@ngrx/effects';
+import { Effects } from './effects';
 
 @NgModule({
   declarations: [
@@ -22,9 +30,16 @@ import { RepositoryComponent } from './components/repository/repository.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MaterialModule
+    MaterialModule,
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10,
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot(Effects)
   ],
   providers: [],
   bootstrap: [AppComponent]
