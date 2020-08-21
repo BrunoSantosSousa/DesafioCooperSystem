f956036f9bb6c55ea2110f88beecb233ccacb31c
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-08-21 08:02:20 -0300

    Importando toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatToolbarModule
   ],
   exports: [
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
