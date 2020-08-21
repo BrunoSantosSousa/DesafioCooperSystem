122d86611ac42951a932c49d1a805f933e9c598f
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-08-21 20:58:11 -0300

    Criado model para repository

---
+++ src/app/models/repository.model.ts
@@ -0,0 +1,12 @@
+export interface Repository {
+    name : string;
+    full_name : string;
+    fork : boolean;
+    html_url : string;
+    stargazers_count : number;
+    language: string;
+    watchers : number;
+    forks : number;
+    created_at : string;
+    updated_at : string;
+}
