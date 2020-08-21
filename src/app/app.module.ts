19bf6a4602e131908573cd986d2559dd05e0ee62
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-08-21 20:57:00 -0300

    Importando modulo http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 
@@ -29,6 +30,7 @@ import { Effects } from './effects';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
