5de448f96a8bc81b82870d860e2878b79d6dfa61
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2020-08-21 20:57:17 -0300

    Refatorando para nomenclatura da API

--- src/app/components/repository/repository.component.ts
+++ src/app/components/repository/repository.component.ts
@@ -8,14 +8,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RepositoryComponent implements OnInit {
 
   @Input('name') name: string;
-  @Input('full-name') fullName: string;
-  @Input('is-fork') isFork: boolean;
-  @Input('html-url') htmlUrl: string;
-  @Input('stargazers') stargazers: number;
+  @Input('full_name') fullName: string;
+  @Input('fork') isFork: boolean;
+  @Input('html_url') htmlUrl: string;
+  @Input('stargazers_count') stargazers: number;
   @Input('watchers') watchers: number;
   @Input('forks') forks: number;
-  @Input('created-at') createdAt: string;
-  @Input('updated-at') updatedAt: string;
+  @Input('created_at') createdAt: string;
+  @Input('updated_at') updatedAt: string;
   
   constructor() { }
 
